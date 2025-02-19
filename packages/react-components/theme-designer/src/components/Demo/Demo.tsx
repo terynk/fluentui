1651e4d4906dd70204316d19e62cd229546f1316
terynkum <terynkum@microsoft.com>
2025-02-18 17:09:00 -0800

    implemented grid layout for Demo

--- packages/react-components/theme-designer/src/components/Demo/Demo.tsx
+++ packages/react-components/theme-designer/src/components/Demo/Demo.tsx
@@ -34,22 +34,36 @@ export interface ContentProps {
 
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  column: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'start',
-    boxSizing: 'border-box',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridTemplateRows: 'repeat(3, auto)',
     gap: tokens.spacingVerticalL,
   },
+  avatar: {
+    gridArea: '1 / 1 / 2 / 2',
+    display: 'flex',
+    gap: tokens.spacingVerticalL,
+  },
+  tabList: {
+    gridArea: '2 / 1 / 3 / 2',
+  },
+  searchAndDropdownField: {
+    gridArea: '3 / 1 / 4 / 2/',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: tokens.spacingVerticalS,
+  },
   controlRow: {
+    gridArea: '1 / 2 / 2 / 3',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'start',
     justifyContent: 'center',
     gap: tokens.spacingHorizontalL,
+  },
+  slider: {
+    gridArea: '2 / 2 / 3 / 3',
+    padding: tokens.spacingVerticalS,
   },
   controlColumn: {
     display: 'flex',
@@ -57,13 +71,33 @@ const useStyles = makeStyles({
     alignItems: 'start',
     justifyContent: 'center',
   },
-  inputLabel: {
+  checkboxAndRadioGroup: {
+    gridArea: '3 / 2 / 4 / 3',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  checkboxGroup: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+    justifyContent: 'center',
+  },
+  radioGroup: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+    justifyContent: 'center',
+  },
+  descriptionField: {
+    gridArea: '1 / 3 / 2 / 4',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'start',
     gap: tokens.spacingVerticalS,
   },
   icons: {
+    gridArea: '2 / 3 / 3 / 4',
     display: 'grid',
     gridTemplateColumns: 'auto auto',
     gridTemplateRows: 'auto auto',
@@ -71,123 +105,88 @@ const useStyles = makeStyles({
     columnGap: tokens.spacingHorizontalS,
     justifyContent: 'center',
   },
-  avatar: {
-    display: 'flex',
-    gap: tokens.spacingVerticalL,
-  },
-  avatarText: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'left',
+  link: {
+    gridArea: '3 / 3 / 4 / 4',
+    alignSelf: 'center',
   },
 });
 
+const MeetNowIcon = bundleIcon(MeetNowFilled, MeetNowRegular);
+const CalendarLtrIcon = bundleIcon(CalendarLtrFilled, CalendarLtrRegular);
 /*
-	Note the state of the stickersheet is that we need consensus on the following post Build 2023:
-		1) Do we have all the correct components displayed and in the correct order?
-		2.a) Do we have all the states of each component displayed? i.e. missing deactive states?
-		2.b) Do was have all the variants of each component displayed? i.e. different sizes, colors, layouts etc?
-		3) Note that the spinner was removed since it was causing confusing with the loading state of the page
+  Note the state of the stickersheet is that we need consensus on the following post Build 2023:
+    1) Do we have all the correct components displayed and in the correct order?
+    2.a) Do we have all the states of each component displayed? i.e. missing deactive states?
+    2.b) Do was have all the variants of each component displayed? i.e. different sizes, colors, layouts etc?
+    3) Note that the spinner was removed since it was causing confusing with the loading state of the page
 */
 
-export const Column1 = () => {
-  const styles = useStyles();
+export const Demo: React.FC<ContentProps> = props => {
+  const stickerSheetStyles = useStyles();
   const dropdownId = useId('dropdown-default');
+
   return (
-    <div className={styles.column}>
-      <div className={styles.avatar}>
+    <div className={mergeClasses(stickerSheetStyles.root, props.className)}>
+      <div className={stickerSheetStyles.avatar}>
         <Persona
           name="Cameron Evans"
           secondaryText="Senior Researcher at Contoso"
           avatar={{ color: 'brand', badge: { status: 'available' } }}
         />
       </div>
-      <TabList defaultSelectedValue="tab1">
-        <Tab value="tab1">Home</Tab>
-        <Tab value="tab2">Pages</Tab>
-        <Tab value="tab3">Documents</Tab>
-      </TabList>
-      <Field>
-        <Input
-          placeholder="Find"
-          contentAfter={<Button aria-label="Find" appearance="transparent" icon={<SearchRegular />} size="small" />}
-        />
-      </Field>
-      <Dropdown aria-labelledby={dropdownId} placeholder="Select" inlinePopup>
-        <Option value="Action 1">Action 1</Option>
-        <Option value="Action 2">Action 2 </Option>
-        <Option value="Action 3">Action 3</Option>
-      </Dropdown>
-    </div>
-  );
-};
-
-export const Column2 = () => {
-  const styles = useStyles();
-  return (
-    <div className={styles.column}>
-      <div className={styles.controlRow}>
+      <div className={stickerSheetStyles.tabList}>
+        <TabList defaultSelectedValue="tab1">
+          <Tab value="tab1">Home</Tab>
+          <Tab value="tab2">Pages</Tab>
+          <Tab value="tab3">Documents</Tab>
+        </TabList>
+      </div>
+      <div className= {stickerSheetStyles.searchAndDropdownField}>
+        <Field>
+          <Input
+            placeholder="Find"
+            contentAfter={<Button aria-label="Find" appearance="transparent" icon={<SearchRegular />} size="small" />}
+          />
+        </Field>
+        <Dropdown aria-labelledby={dropdownId} placeholder="Select" inlinePopup>
+          <Option value="Action 1">Action 1</Option>
+          <Option value="Action 2">Action 2</Option>
+          <Option value="Action 3">Action 3</Option>
+        </Dropdown>
+      </div>
+      <div className={stickerSheetStyles.controlRow}>
         <Button appearance="primary">Text</Button>
-        <div className={styles.controlColumn}>
+        <div className={stickerSheetStyles.controlColumn}>
           <Switch defaultChecked={true} label="On" />
           <Switch label="Off" />
         </div>
       </div>
-      <Slider defaultValue={50} />
-      <div className={styles.controlRow}>
-        <div className={styles.controlColumn}>
+      <Slider className={stickerSheetStyles.slider} defaultValue={50} />
+      <div className={stickerSheetStyles.checkboxAndRadioGroup}>
+        <div className={stickerSheetStyles.checkboxGroup}>
           <Checkbox defaultChecked={true} label="Option 1" />
           <Checkbox label="Option 2" />
         </div>
-        <div className={styles.controlColumn}>
+        <div className={stickerSheetStyles.radioGroup}>
           <RadioGroup>
             <Radio defaultChecked={true} label="Option 1" />
             <Radio label="Option 2" />
           </RadioGroup>
         </div>
       </div>
-    </div>
-  );
-};
-
-// We may use these later.
-export const DemoIcons = () => {
-  const styles = useStyles();
-  const MeetNowIcon = bundleIcon(MeetNowFilled, MeetNowRegular);
-  const CalendarLtrIcon = bundleIcon(CalendarLtrFilled, CalendarLtrRegular);
-  return (
-    <div className={styles.icons}>
-      <Badge size="medium" appearance="filled" icon={<CalendarLtrIcon />} />
-      <Badge size="medium" appearance="ghost" icon={<CalendarLtrIcon />} />
-      <Badge size="medium" appearance="outline" icon={<MeetNowIcon />} />
-      <Badge size="medium" appearance="tint" icon={<MeetNowIcon />} />
-    </div>
-  );
-};
-
-export const Column3 = () => {
-  const styles = useStyles();
-
-  return (
-    <div className={styles.column}>
-      <div className={styles.inputLabel}>
+      <div className={stickerSheetStyles.descriptionField}>
         <Field label="Description" required>
           <Input placeholder="Example Text" appearance="filled-darker" />
         </Field>
       </div>
-      <Link href="https://www.microsoft.com">Example link - www.microsoft.com</Link>
-    </div>
-  );
-};
-
-export const Demo: React.FC<ContentProps> = props => {
-  const styles = useStyles();
-  return (
-    <div>
-      <div className={mergeClasses(styles.root, props.className)}>
-        <Column1 />
-        <Column2 />
-        <Column3 />
+      <Link className={stickerSheetStyles.link} href="https://www.microsoft.com">
+        Example link - www.microsoft.com
+      </Link>
+      <div className={stickerSheetStyles.icons}>
+        <Badge size="extra-large" appearance="filled" icon={<CalendarLtrIcon />} />
+        <Badge size="extra-large" appearance="ghost" icon={<CalendarLtrIcon />} />
+        <Badge size="extra-large" appearance="outline" icon={<MeetNowIcon />} />
+        <Badge size="extra-large" appearance="tint" icon={<MeetNowIcon />} />
       </div>
     </div>
   );
